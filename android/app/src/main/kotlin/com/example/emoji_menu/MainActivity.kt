c3a0b479374ca925362448c2ad76a266f639b094
unknown <matthewgdasilva25@gmail.com>
2020-05-13 17:05:49 +0200

    done

---
+++ android/app/src/main/kotlin/com/example/emoji_menu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emoji_menu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,215 @@
+import 'package:emoji_menu/screens/food_tabs.dart';
+import 'package:emoji_menu/screens/itempage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashBoardScreen extends StatefulWidget {
+
+  @override
+  _DashBoardScreenState createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> with SingleTickerProviderStateMixin{
+
+  TabController tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    tabController = TabController(vsync: this, length: 4);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.menu, color: Colors.black,),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        blurRadius: 6.0,
+                        spreadRadius: 4.0,
+                        offset: Offset(0.0,3.0),
+                      ),
+                    ],
+                    color: Color(0xFFC6E7FE),
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/tuxedo.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0,),
+            child: Text(
+              'Search For',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 27.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0,),
+            child: Text(
+              'Recipes',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 27.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 25.0,),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: TextStyle(
+                    fontSize: 14.0
+                  ),
+                  border: InputBorder.none,
+                  fillColor: Colors.grey.withOpacity(0.5),
+                  prefixIcon: Icon(Icons.search, color: Colors.grey,),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: Text(
+              'Recommended',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Container(
+            height: 200.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _buildListItem('Hamburger', 'assets/images/burger.png', '10', Colors.orangeAccent.withOpacity(0.3), Colors.orange),
+                _buildListItem('Fires', 'assets/images/fries.png', '10', Colors.blueAccent.withOpacity(0.3), Colors.blue),
+                _buildListItem('Doughnut', 'assets/images/doughnut.png', '10', Colors.greenAccent.withOpacity(0.3), Colors.green),
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: TabBar(
+              controller: tabController,
+              isScrollable: true,
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.withOpacity(0.5),
+              labelStyle: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+              tabs: <Widget>[
+                Tab(child: Text('Featured'),),
+                Tab(child: Text('Combo'),),
+                Tab(child: Text('Favorites'),),
+                Tab(child: Text('Recommended'),),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 450,
+            child: TabBarView(
+              controller: tabController,
+              children: <Widget>[
+                FoodTab(),
+                FoodTab(),
+                FoodTab(),
+                FoodTab(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  _buildListItem(String foodName, String img, String price, Color c,Color textColor){
+    return Padding(
+      padding: EdgeInsets.only(left: 15.0),
+      child: InkWell(
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ItemPage(img,foodName,price,foodName),
+          ));
+        },
+        child: Container(
+          height: 175.0,
+          width: 150.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: c,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: foodName,
+                child: Container(
+                  height: 75.0,
+                  width: 75.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: Image.asset(img, height: 50.0, width: 50.0,),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25.0,),
+              Text(
+                foodName,
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+              Text(
+                '\$'+price,
+                style: TextStyle(
+                  fontSize: 17.0,
+                  color: textColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
